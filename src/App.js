8d155d185d692ff3bd5a783ce9118f2e5ef49e8e
hendra-agil <agilsyaputra26@gmail.com>
2020-08-25 19:31:03 +0700

    Route Parameters #1

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
+import Post from './components/Post';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route exact path="/" component={Home} />
         <Route path="/about" component={About} />
         <Route path="/contact" component={Contact} />
+        <Route path="/:post_id" component={Post} />
       </div>
     </BrowserRouter>
   );
