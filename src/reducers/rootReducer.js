f15f8b02bd889ed15e06338ddeac477477044105
hendra-agil <agilsyaputra26@gmail.com>
2020-08-26 19:35:07 +0700

    Mapping State to Props

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,5 +1,24 @@
 const initState = {
-  posts: [],
+  posts: [
+    {
+      id: '1',
+      title: 'Nullam scelerisque felis mauris',
+      body:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar fringilla eleifend. Maecenas ac venenatis nunc, scelerisque volutpat magna. In faucibus mollis ante nec euismod. Nullam eget congue orci, eget imperdiet ligula. Morbi nec mauris metus. Donec viverra enim odio, at vestibulum ante rhoncus semper. Pellentesque sem justo, bibendum et ullamcorper sit amet, aliquet id orci.',
+    },
+    {
+      id: '2',
+      title: 'Pellentesque non lacinia leo',
+      body:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar fringilla eleifend. Maecenas ac venenatis nunc, scelerisque volutpat magna. In faucibus mollis ante nec euismod. Nullam eget congue orci, eget imperdiet ligula. Morbi nec mauris metus. Donec viverra enim odio, at vestibulum ante rhoncus semper. Pellentesque sem justo, bibendum et ullamcorper sit amet, aliquet id orci.',
+    },
+    {
+      id: '3',
+      title: 'Nullam sit amet malesuada',
+      body:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec pulvinar fringilla eleifend. Maecenas ac venenatis nunc, scelerisque volutpat magna. In faucibus mollis ante nec euismod. Nullam eget congue orci, eget imperdiet ligula. Morbi nec mauris metus. Donec viverra enim odio, at vestibulum ante rhoncus semper. Pellentesque sem justo, bibendum et ullamcorper sit amet, aliquet id orci.',
+    },
+  ],
 };
 
 const rootReducer = (state = initState, action) => {
