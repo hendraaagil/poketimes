828fd20b24764840b25ec84d144d1e8ab37bf227
hendra-agil <agilsyaputra26@gmail.com>
2020-08-26 20:12:02 +0700

    Match Dispatch to Props

--- src/components/Post.js
+++ src/components/Post.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class Post extends Component {
+  handleClick = () => {
+    this.props.deletePost(this.props.post.id);
+    this.props.history.push('/');
+  };
+
   render() {
     const post = this.props.post ? (
       <div className="post">
         <h4 className="center">{this.props.post.title}</h4>
         <p>{this.props.post.body}</p>
+        <div className="center">
+          <button className="btn red" onClick={this.handleClick}>
+            Delete Post
+          </button>
+        </div>
       </div>
     ) : (
       <div className="center">Loading post...</div>
@@ -23,4 +33,12 @@ const mapStateToProps = (state, ownProps) => {
   };
 };
 
-export default connect(mapStateToProps)(Post);
+const mapDispatchToProps = (dispatch) => {
+  return {
+    deletePost: (id) => {
+      dispatch({ type: 'DELETE_POST', id: id });
+    },
+  };
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(Post);
--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -22,6 +22,15 @@ const initState = {
 };
 
 const rootReducer = (state = initState, action) => {
+  if (action.type === 'DELETE_POST') {
+    let newPosts = state.posts.filter((post) => {
+      return action.id !== post.id;
+    });
+    return {
+      ...state,
+      posts: newPosts,
+    };
+  }
   return state;
 };
 
